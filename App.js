3beb648ec949115e397044f30156ea42d28aa47b
yonureker <yonureker@gmail.com>
2019-12-03 00:17:53 -0800

    add FlatList

--- App.js
+++ App.js
@@ -1,19 +1,50 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Button,
+  TextInput,
+  FlatList
+} from "react-native";
 
 export default function App() {
-  const [ outputText, setOutputText ] = useState('Open up App.js to start working')
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [goalList, setGoalList] = useState([]);
+
+  const inputHandler = inputText => {
+    setEnteredGoal(inputText);
+  };
+
+  const buttonSubmitHandler = () => {
+    setGoalList(currentGoals => [
+      ...currentGoals,
+      { id: Math.random().toString(), value: enteredGoal }
+    ]);
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.inputRow}>
-        <TextInput placeholder="Course Goal" style={styles.textInput}/>
-        <Button title="ADD"/>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.textInput}
+          onChangeText={inputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={buttonSubmitHandler} />
       </View>
 
-      <View>
-
-      </View>
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={goalList}
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      ></FlatList>
     </View>
   );
 }
@@ -23,14 +54,21 @@ const styles = StyleSheet.create({
     padding: 50
   },
   textInput: {
-    borderColor: 'black',
+    borderColor: "black",
     borderWidth: 1,
     padding: 10,
-    width: '80%'
+    width: "80%"
   },
   inputRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  listItem: {
+    padding: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
+    margin: 0
   }
 });
